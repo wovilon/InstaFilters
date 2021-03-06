29fe3f576a6a1b443cf464e8667dbec50620d361
Vladimir <wwwovilon@gmail.com>
2021-03-06 20:43:59 +0200

    image added

--- app/src/main/java/com/example/instafilters/MainActivity.kt
+++ app/src/main/java/com/example/instafilters/MainActivity.kt
@@ -1,12 +1,25 @@
 package com.example.instafilters
 
-import androidx.appcompat.app.AppCompatActivity
+import android.graphics.Bitmap
+import android.graphics.BitmapFactory
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.mukesh.image_processing.ImageProcessor
+import kotlinx.android.synthetic.main.activity_main.*
+
 
 class MainActivity : AppCompatActivity() {
+    lateinit var bitmap : Bitmap
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+
+        bitmap = BitmapFactory.decodeResource(resources, R.drawable.image);
+
+
 
 
     }
