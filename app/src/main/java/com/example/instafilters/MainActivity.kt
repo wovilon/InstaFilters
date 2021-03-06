2a481fc757ce07f1337a93e8bc2322a5b5593c27
Vladimir <wwwovilon@gmail.com>
2021-03-06 20:45:36 +0200

    lib inserted

--- app/src/main/java/com/example/instafilters/MainActivity.kt
+++ app/src/main/java/com/example/instafilters/MainActivity.kt
@@ -10,16 +10,17 @@ import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     lateinit var bitmap : Bitmap
-
+    lateinit var oneBitMap : Bitmap
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        bitmap = BitmapFactory.decodeResource(resources, R.drawable.image)
 
-        bitmap = BitmapFactory.decodeResource(resources, R.drawable.image);
-
-
+        val processor = ImageProcessor()
+        oneBitMap = processor.tintImage(bitmap, 90)
+        ivMainImage.setImageBitmap(oneBitMap)
 
 
     }
