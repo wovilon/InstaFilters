19fbf2869dcf47e334d0fc48cee456a9b4c3971a
Vladimir <wwwovilon@gmail.com>
2021-03-07 22:16:23 +0200

    load image from camera + ui updates

--- app/src/main/java/com/example/instafilters/MainActivity.kt
+++ app/src/main/java/com/example/instafilters/MainActivity.kt
@@ -1,8 +1,10 @@
 package com.example.instafilters
 
+import android.content.Intent
 import android.graphics.Bitmap
 import android.graphics.BitmapFactory
 import android.os.Bundle
+import android.provider.MediaStore
 import androidx.appcompat.app.AppCompatActivity
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.mukesh.image_processing.ImageProcessor
@@ -12,10 +14,19 @@ import kotlinx.android.synthetic.main.activity_main.*
 class MainActivity : AppCompatActivity() {
     lateinit var bitmap : Bitmap
     lateinit var oneBitMap : Bitmap
+    val PICK_IMAGE = 100
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+
+        val intent: Intent = Intent()
+        intent.setType("image/*")
+        intent.setAction(Intent.ACTION_GET_CONTENT)
+
+
+
 
         bitmap = BitmapFactory.decodeResource(resources, R.drawable.image)
 
@@ -23,6 +34,15 @@ class MainActivity : AppCompatActivity() {
         oneBitMap = processor.tintImage(bitmap, 90)
         ivMainImage.setImageBitmap(oneBitMap)
 
+        initAdapter()
+
+        btGallery.setOnClickListener {
+            startActivityForResult(Intent.createChooser(intent, "Select Picture"), PICK_IMAGE)
+        }
+
+    }
+
+    fun initAdapter(){
         val images = ArrayList<FilterItem>()
         images.add(FilterItem("OneFilter", bitmap))
         images.add(FilterItem("OneFilter", bitmap))
@@ -33,7 +53,16 @@ class MainActivity : AppCompatActivity() {
         val adapter = FilterAdapter(images, this)
         rvFilters.layoutManager = LinearLayoutManager(this, LinearLayoutManager.HORIZONTAL, false)
         rvFilters.adapter = adapter
+    }
 
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if (resultCode == RESULT_OK && requestCode == PICK_IMAGE){
+            val imageUri = data?.getData()
+            //ivMainImage.setImageURI(imageUri)
+            bitmap = MediaStore.Images.Media.getBitmap(this.getContentResolver(), imageUri);
+
+        }
     }
 
 }
